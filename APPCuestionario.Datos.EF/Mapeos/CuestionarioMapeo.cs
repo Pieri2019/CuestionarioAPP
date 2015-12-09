1dda9aecee164480328b5ec621cfacd43801ad03
Maauricio <daniel_5714@hotmail.com>
2015-12-09 21:51:14 +0100

    Mapeo Cuestionario

---
+++ APPCuestionario.Datos.EF/Mapeos/CuestionarioMapeo.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace APPCuestionario.Datos.EF.Mapeos
+{
+    class CuestionarioMapeo : EntityTypeConfiguration<Cuestionario>
+    {
+
+        public CuestionarioMapeo()
+        {
+            ToTable("CUESTIONARIOS", "CS");
+            HasKey(k => k.IDCuestionario);
+            Property(p => p.IDCuestionario).HasColumnName("CUESTIONARIO_ID").IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            Property(p => p.IDUsuario).HasColumnName("USUARIO_ID").IsRequired();
+            Property(p => p.Descripcion).HasColumnName("DESCRIPCION").IsRequired();
+            Property(p => p.EstadoCuestionario).HasColumnName("ESTADO").IsRequired();
+            Property(p => p.)
+
+            HasRequired(m => m.Creador).WithMany().HasForeignKey(f => f.IDUsuario);
+           
+        }
+
+    }
+}
