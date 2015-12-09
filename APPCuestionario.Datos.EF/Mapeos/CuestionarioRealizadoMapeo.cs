214f7998c89b43c68d1315a43137d9e39a220937
TAvanzados <pierinaa_10@hotmail.com>
2015-12-09 22:09:05 +0100

    Cambio mapeo cuestionario realizado

---
+++ APPCuestionario.Datos.EF/Mapeos/CuestionarioRealizadoMapeo.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Data.Entity.ModelConfiguration;
+
+namespace APPCuestionario.Datos.EF.Mapeos
+{
+    class CuestionarioRealizadoMapeo : EntityTypeConfiguration<CuestionarioRealizado>
+
+
+    {
+
+        public CuestionarioRealizadoMapeo()
+        {
+            ToTable("CUESTIONARIO_REALIZADO", "CS");
+            HasKey(k => k.IDCuestionarioRealizado);
+            Property(p => p.IDCuestionarioRealizado).HasColumnName("CUESTIONARIOREALI_ID").IsRequired();
+            Property(p => p.FechaInicio).HasColumnName("CUESTIONARIOREALI_FECHAINICIO").IsRequired();
+            Property(p => p.FechaTermino).HasColumnName("CUESTIONARIOREALI_FECHAFIN").IsRequired();
+            Property(p => p.IDCuestionario).HasColumnName("CUESTIONARIO_ID").IsRequired();
+            Property(p => p.IDUsuario).HasColumnName("USUARIO_ID").IsRequired();
+
+            HasRequired(m => m.Cuestionario).WithMany().HasForeignKey(f => f.IDCuestionario);
+            HasRequired(m => m.Usuario ).WithMany().HasForeignKey(f => f.IDUsuario);
+        }
+    }
+}
