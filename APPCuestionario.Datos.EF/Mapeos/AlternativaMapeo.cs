769671999c3dbe7403ce2b8303f26c9aa992f917
renzorl89 <renzorl.89@hotmail.com>
2015-12-09 21:36:01 +0100

    AlternativaMapeo

---
+++ APPCuestionario.Datos.EF/Mapeos/AlternativaMapeo.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Data.Entity.ModelConfiguration;
+namespace APPCuestionario.Datos.EF.Mapeos
+{
+    class AlternativaMapeo : EntityTypeConfiguration<Alternativa>
+
+    {
+        public AlternativaMapeo()
+        {
+            ToTable("ALTERNATIVAS", "");
+            HasKey(k => k.IDAlternativa);
+            Property(p => p.IDAlternativa).HasColumnName("ALTERNATIVA_ID").IsRequired();
+            Property(p => p.Descripcion).HasColumnName("DESCRIPCION").IsRequired();
+            Property(p => p.EsCorrecta).HasColumnName("CORRECTA").IsRequired();
+            Property(p => p.IDPregunta).HasColumnName("PREGUNTA_ID").IsRequired();
+
+            HasRequired(m => m.Pregunta).WithMany().HasForeignKey(f => f.IDPregunta);
+        }
+
+    }
+}
