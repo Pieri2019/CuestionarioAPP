43d4b794ed2c8e37cd9d6a1d9af6633473267119
giordya <giordy2a@hotmail.com>
2015-12-11 21:40:57 +0100

    Cambio Repositorio

---
+++ APPCuestionario/Repositorios/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPCuestionario.Repositorios
+{
+    public interface IRepositorio<T> : IRepositorioLectura<T>
+
+    {
+        bool Adicionar(T ao_entidad);
+        bool Actualizar(T aao_entidad);
+        bool GuardarCambios();
+    }
+}
---
+++ APPCuestionario/Repositorios/IRepositorioLectura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPCuestionario.Repositorios
+{
+    public interface IRepositorioLectura<T>
+    {
+        T ObtenerPorCodigo(params object[] ao_llaves);
+        IQueryable<T> Listas();
+        IList<T> ObtenerPorExpresion(Expression<Func<T, bool>> ao_llaves = null, string as_incluir - null, byte aby_limite = 0);
+    }
+}
